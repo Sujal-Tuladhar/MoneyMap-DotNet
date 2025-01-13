6ba51e1438be8dadc1b04e9b695ff070811862e8
Sujal-Tuladhar <tuladhar.sujal10@gmail.com>
2025-01-13 20:04:09 +0545

    Inital Commit

---
+++ MoneyMapDotnet/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyMapDotnet;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MoneyMapDotnet/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MoneyMapDotnet;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
